5c355a930d48edff4a0c1b59285db545b6a36bc9
Logan Duffy <duffylogan@outlook.com>
2023-11-21 12:24:28 +0100

    .

--- Cannon/Game1.cs
+++ Cannon/Game1.cs
@@ -11,6 +11,8 @@ namespace Cannon
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        //cannon "trajectory line" variables
+
         //the positions that make up the "trajectory line"
         Vector2 cannonLineStartPos;
         Vector2 cannonLineEndPos;
@@ -20,7 +22,14 @@ namespace Cannon
         Color cannonLineColor;
 
         //input your own angle to determine the "trajectory" line of the cannon
-        float cannonLineAngle;
+        float[] cannonAngles;
+
+        //cannon variables
+
+        Vector2 cannonStartPos;
+
+        int cannonThickness;
+        Color cannonColor;
 
         public Game1()
         {
@@ -37,18 +46,22 @@ namespace Cannon
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            
 
+            //input custom angle to update the trajectory of the cannon and it's "laser"
+            cannonAngles = new float[] {0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90 };
+
+ 
             //sets cannon "trajectory" line in the middle of the game window pointing upwards to the right
             cannonLineStartPos = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2);
-            cannonLineEndPos = new Vector2(cannonLineStartPos.X + 100, cannonLineStartPos.Y + 100);
+            cannonLineEndPos = new Vector2(cannonLineStartPos.X + 100, cannonLineStartPos.Y - 100);
 
             //cannon "trajectory" line aesthetics
             cannonLineThickness = 5;
             cannonLineColor = Color.Red;
 
-            //angle of the cannon's "trajectory" line
-            cannonLineAngle = 45;
+            cannonStartPos = new Vector2(cannonLineStartPos.X - 50, cannonLineStartPos.Y + 50);
+            cannonThickness = 20;
+            cannonColor = Color.White;
         }
 
         protected override void Update(GameTime gameTime)
@@ -66,8 +79,11 @@ namespace Cannon
             _spriteBatch.Begin();
 
             //method to draw out the "trajectory" line.
-            DrawLine(_spriteBatch, cannonLineStartPos, cannonLineEndPos, cannonLineThickness, cannonLineColor, cannonLineAngle);
-
+            foreach (var angle in cannonAngles)
+            {
+                DrawLine(_spriteBatch, cannonLineStartPos, cannonLineEndPos, cannonLineThickness, cannonLineColor, angle);
+            }
+            DrawLine(_spriteBatch, cannonStartPos, cannonLineStartPos, cannonThickness, cannonColor, 45);
             _spriteBatch.End();
 
             base.Draw(gameTime);
@@ -94,20 +110,26 @@ namespace Cannon
             if (angle > 90)
             {
                 //A cannon can't go above 90 degrees, therefore if user puts in higher than 90, it defaults to it's maximum possible degree
-                rotation = MathHelper.ToRadians(-90); //set angle to negative to invert trajectory (Monogame's positive y-axis points downward)
+                rotation = ConvertDegreesToRadians(-90); //set angle to negative to invert trajectory (Monogame's positive y-axis points downward)
             }
             else if (angle < 0)
             {
                 //if user inputs an angle lower than 0 (horizontal plane) it defaults to a straight line because a cannon can't shoot downwards.
-                rotation = MathHelper.ToRadians(0);
+                rotation = ConvertDegreesToRadians(0); //set angle to negative to invert trajectory (Monogame's positive y-axis points downward)
             }
             else
             {
-                rotation = MathHelper.ToRadians(-angle); //set angle to negative to invert trajectory (Monogame's positive y-axis points downward)
+                rotation = ConvertDegreesToRadians(-angle); //set angle to negative to invert trajectory (Monogame's positive y-axis points downward)
             }
             var origin = new Vector2(0, thickness / 2);
 
             spriteBatch.Draw(texture, startPos, null, Color.White, rotation, origin, 1f, SpriteEffects.None, 1f);
+        }
+
+        //converts degrees to radians through the normal formula pi through 180
+        public static float ConvertDegreesToRadians(float degrees)
+        {
+            return degrees * ((float)Math.PI / 180);
         }
     }
 }
